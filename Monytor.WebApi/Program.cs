abadccbf4ee821786eb1c3cf12a2060ee6bb36ff
Alex <mystackoverflow@googlemail.com>
2018-07-12 22:53:55 +0200

    WebApi configurable

--- Monytor.WebApi/Program.cs
+++ Monytor.WebApi/Program.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Monytor.WebApi {
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var configuration = new ConfigurationBuilder()
+               .AddCommandLine(args)
+               .Build();
+
+            BuildWebHost(args, configuration)              
+                .Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args, IConfiguration configuration) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(configuration)
                 .UseStartup<Startup>()
                 .Build();
     }
