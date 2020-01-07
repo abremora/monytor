0dfa4dd8f1c021d9d7f9e6c534434488473a76d2
t0ms3n <thomas.schroeder@eckd.de>
2020-01-07 10:28:29 +0100

    Support appsettings.docker.json

--- Monytor.WebApi/Program.cs
+++ Monytor.WebApi/Program.cs
@@ -1,28 +1,30 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 using System.Reflection;
+using Microsoft.Extensions.Hosting;
 
 namespace Monytor.WebApi {
     public class Program {
         public static void Main(string[] args) {
-            var configuration = new ConfigurationBuilder()
-               .AddCommandLine(args)
-               .SetBasePath(GetCurrentDirectory())
-               .AddJsonFile("appsettings.json")
-               .AddJsonFile("appsettings.{env.EnvironmentName}.json", true)
-               .Build();
-
-            BuildWebHost(args, configuration)
-                .Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args, IConfiguration configuration) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseConfiguration(configuration)
-                .UseStartup<Startup>()
-                .Build();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureAppConfiguration((hostingContext, configuration) =>
+                    {
+                        configuration.AddCommandLine(args)
+                            .SetBasePath(GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .AddJsonFile("settings/appsettings.docker.json", true);
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
 
         private static string GetCurrentDirectory() {
             return Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
