a72ba243dec9cbf913f8918ed2dc9d3d45ced419
t0ms3n <thomas.schroeder@eckd.de>
2020-01-14 21:28:11 +0100

    Delete items from table when removed

--- Monytor.Ui/src/app/modules/administration/pages/search-collector-configuration/components/collector-configuration-table/collector-configuration-table.component.ts
+++ Monytor.Ui/src/app/modules/administration/pages/search-collector-configuration/components/collector-configuration-table/collector-configuration-table.component.ts
@@ -1,24 +1,46 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { CollectorConfigSearchResult } from 'src/app/modules/administration/models/collector-config-search-result.model';
 import { Search } from 'src/app/shared/models/search.model';
 import { CollectorConfigApiService } from 'src/app/modules/administration/services/collector-config-api.service';
-import { take } from 'rxjs/operators';
+import { takeUntil, distinct, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'mt-collector-configuration-table',
   templateUrl: './collector-configuration-table.component.html',
   styles: []
 })
-export class CollectorConfigurationTableComponent implements OnInit {
+export class CollectorConfigurationTableComponent implements OnInit, OnDestroy {
   @Input() public collectorConfigurations: Search<CollectorConfigSearchResult>;
+
+  @Output() public collectorDeleted = new EventEmitter<string>();
+
+  private unsubscribe = new Subject();
 
   constructor(private apiSerivce: CollectorConfigApiService) { }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
   public deleteCollectorConfiguration(id: string) {
-    this.apiSerivce.deleteCollectorConfiguration(id).toPromise().then(response => console.log('deleted'), error => console.log(error));
+    this.apiSerivce.deleteCollectorConfiguration(id)
+      .pipe(
+        takeUntil(this.unsubscribe),
+        distinct(),
+        tap(() => {
+          // ToDo: May it is better to just mark is as deleted in search result and hide it, so it does not affect totalResults
+          this.collectorDeleted.emit(id);
+          this.collectorConfigurations.items = this.collectorConfigurations.items.filter(item => item.collectorConfigId !== id);
+        })
+      )
+      .subscribe({
+        error: err => console.error(err)
+      });
   }
 
 }
