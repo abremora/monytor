1930f6ce368ecb715885dede90c4e85fe381de75
t0ms3n <thomas.schroeder@eckd.de>
2020-01-14 21:19:07 +0100

    navigate to edit after creating collectorconfig

--- Monytor.Ui/src/app/modules/administration/pages/add-collector-configuration/add-collector-configuration.component.ts
+++ Monytor.Ui/src/app/modules/administration/pages/add-collector-configuration/add-collector-configuration.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { CollectorConfigApiService } from '../../services/collector-config-api.service';
 import { CreateCollectorConfigCommand } from '../../models/create-collector-config-command.model';
 import { CollectorConfigurationFormData } from '../../models/collector-configuration-form-data.model';
+import { take, tap, takeUntil } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'mt-add-collector-configuration',
   templateUrl: './add-collector-configuration.component.html',
   styles: []
 })
-export class AddCollectorConfigurationComponent implements OnInit {
+export class AddCollectorConfigurationComponent implements OnInit, OnDestroy {
 
 
-  constructor(private apiService: CollectorConfigApiService) { }
+  private unsubscribe = new Subject();
+
+  constructor(private apiService: CollectorConfigApiService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
   public onSubmit(event: CollectorConfigurationFormData) {
@@ -22,8 +32,17 @@ export class AddCollectorConfigurationComponent implements OnInit {
       .createCollectorConfig(new CreateCollectorConfigCommand(
         event.displayName
         , event.schedulerAgent))
-      .toPromise()
-      .then(response => console.log(response), error => console.log(error));
+      .pipe(
+        takeUntil(this.unsubscribe),
+        take(1),
+        tap(id => {
+          this.router.navigateByUrl(`/administration/edit/${encodeURIComponent(id)}`, {
+            replaceUrl: true
+          });
+        }),
+      ).subscribe({
+        error: (err) => console.error(err)
+      });
   }
 
 }
