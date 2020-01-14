3741f67558a5f3032fa19b889cfc1bdfaffb235c
t0ms3n <thomas.schroeder@eckd.de>
2020-01-14 20:54:17 +0100

    set active navbar item

---
+++ Monytor.Ui/src/app/core/navbar/nav-item.model.ts
@@ -0,0 +1,4 @@
+﻿export interface NavItem {
+    displayText: string;
+    route: string;
+}
--- Monytor.Ui/src/app/core/navbar/navbar.component.ts
+++ Monytor.Ui/src/app/core/navbar/navbar.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { NavItem } from './nav-item.model';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'mt-navbar',
     templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
-    constructor() {
+
+    public navItems: NavItem[] = [];
+    public activeNavItem: NavItem;
+
+    constructor(private router: Router) {
     }
 
-    ngOnInit() {
+    public ngOnInit(): void {
+        this.navItems.push({ displayText: 'Dashboard', route: '/dashboard' });
+        this.navItems.push({ displayText: 'Administration', route: '/administration' });
+        // ToDo: Set initial NavItem by current route
+        this.activeNavItem = this.navItems[0];
+    }
 
+    public navigateTo(navItem: NavItem): void {
+        if (navItem === this.activeNavItem) {
+            return;
+        }
+        this.activeNavItem = navItem;
+        this.router.navigateByUrl(navItem.route);
     }
 }
